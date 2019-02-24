cf90a1a8d1f4b802fdb5af7aada921581fa29547
thony <antonprimako@gmail.com>
2019-02-24 21:18:24 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // TODO Initialize Api Context
+        ApiContextInitializer.init();
+
+        // TODO Instantiate Telegram Bots API
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        // TODO Register our bot
+        try {
+            botsApi.registerBot(new JamiumBot());
+//            botsApi.registerBot((LongPollingBot) new JamiumBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
