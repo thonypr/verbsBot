b5742a31f738be4c3c3e2426f4ac6bbe6eb944a5
thony <antonprimako@gmail.com>
2019-03-02 21:08:14 +0300

    reply inline button

--- src/main/java/JamiumBot.java
+++ src/main/java/JamiumBot.java
@@ -1,7 +1,12 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JamiumBot extends TelegramLongPollingBot {
 
@@ -13,9 +18,22 @@ public class JamiumBot extends TelegramLongPollingBot {
             String message_text = update.getMessage().getText();
             long chat_id = update.getMessage().getChatId();
 
-            SendMessage message = new SendMessage() // Create a message object object
-                    .setChatId(chat_id)
-                    .setText(message_text);
+            //SendMessage message = new SendMessage() // Create a message object object
+            //        .setChatId(chat_id)
+            //        .setText(message_text);
+
+            SendMessage message = new SendMessage();
+            message.setChatId(chat_id);
+            message.setText(message_text);
+            InlineKeyboardMarkup options = new InlineKeyboardMarkup();
+            InlineKeyboardButton button = new InlineKeyboardButton("Hey! I'm inline button!");
+            List<InlineKeyboardButton> b1 = new ArrayList<InlineKeyboardButton>();
+            b1.add(button);
+            List<List<InlineKeyboardButton>> buttons = new ArrayList<List<InlineKeyboardButton>>();
+            buttons.add(b1);
+            options.setKeyboard(buttons);
+            message.setReplyMarkup(options);
+
             try {
                 execute(message); // Sending our message object to user
             } catch (TelegramApiException e) {
